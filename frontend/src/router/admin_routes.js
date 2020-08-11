0e90f37c39839ca75584d0f55463c60dc8248d4d
Ritik Taneja <ritiktaneja10@gmail.com>
2020-08-12 01:30:41 +0530

    Created admin_routes config

---
+++ frontend/src/router/admin_routes.js
@@ -0,0 +1,34 @@
+
+import ServicesList from '../components/Admin/ServicesList'
+import Users from '../components/Admin/Users'
+
+
+const admin_routes = [
+    
+    {
+      path : 'services',
+      name : 'Admin.Services',
+      component :  ServicesList,    
+      meta : {
+            title : 'PS Admin | Services',
+            
+        }
+    },
+
+    {
+        path : 'users',
+        name : 'Admin.Users',
+        component : Users,
+        meta : {
+            title : 'PS Admin | User List'
+        }
+    },
+
+    {
+        path : 'groups',
+        name : 'Admin.Groups',
+       
+    }
+]
+
+export default admin_routes
