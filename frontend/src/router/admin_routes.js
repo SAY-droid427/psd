5543f4a0b1a17eb3796f010a209dcb420ca65100
Kanishq Khandelwal <kani@pop-os.localdomain>
2020-08-13 21:05:36 +0530

    User Page

--- frontend/src/router/admin_routes.js
+++ frontend/src/router/admin_routes.js
@@ -1,34 +1,29 @@
-
-import ServicesList from '../components/Admin/ServicesList'
-import Users from '../components/Admin/Users'
-
+import ServicesList from "../components/Admin/ServicesList";
+import Users from "../components/Admin/User/Users.vue";
 
 const admin_routes = [
-    
-    {
-      path : 'services',
-      name : 'Admin.Services',
-      component :  ServicesList,    
-      meta : {
-            title : 'PS Admin | Services',
-            
-        }
-    },
+	{
+		path: "services",
+		name: "Admin.Services",
+		component: ServicesList,
+		meta: {
+			title: "PS Admin | Services",
+		},
+	},
 
-    {
-        path : 'users',
-        name : 'Admin.Users',
-        component : Users,
-        meta : {
-            title : 'PS Admin | User List'
-        }
-    },
+	{
+		path: "users",
+		name: "Admin.Users",
+		component: Users,
+		meta: {
+			title: "PS Admin | User List",
+		},
+	},
 
-    {
-        path : 'groups',
-        name : 'Admin.Groups',
-       
-    }
-]
+	{
+		path: "groups",
+		name: "Admin.Groups",
+	},
+];
 
-export default admin_routes
+export default admin_routes;
