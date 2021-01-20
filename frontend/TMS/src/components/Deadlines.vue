4a6f29e8be2351a35c504d15fac6870382f58f46
SAY-droid427 <sayanimallick0218@gmail.com>
2021-01-20 23:28:54 +0530

    Completed the Transfer Status Page

---
+++ frontend/TMS/src/components/Deadlines.vue
@@ -0,0 +1,38 @@
+<template>   
+    <div class="container">       
+         <h3 v-for="deadline in deadlines" v-bind:key="deadline.id" >{{deadline.id}}. Deadline for {{deadline.subject}}-Date:{{deadline.date}} Time:{{deadline.time}}</h3>   
+    </div>        
+
+</template>
+
+<script>
+export default {
+    name:"Deadlines",
+    data:function(){
+        return{
+            deadlines:[
+                {
+                    id:1,
+                    subject:"PS to TS transfer Application",
+                    date:"2021-02-04",
+                    time:"23:59:59"
+                },
+                {
+                    id:2,
+                    subject:"TS to PS Transfer Application",
+                    date:"2021-02-05",
+                    time:"13:00:00"
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+.container {
+    background-color: rgb(185, 222, 246);
+    text-align: center;
+}
+
+</style>
---
+++ frontend/TMS/src/components/StatusComments.vue
@@ -0,0 +1,47 @@
+<template>
+      <v-form>
+    <v-container>
+      <v-row v-for="comment in comments" v-bind:key="comment.id">
+            <v-col cols="4"><strong>{{comment.authority}}</strong></v-col>
+        <v-col cols="8">
+          <v-text-field height="150"
+            placeholder=""
+            outlined filled
+          ></v-text-field>  
+        </v-col>     
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+export default {
+    name:"StatusComments",
+    data:function(){
+        return{
+            comments:[
+                {
+                    id:1,
+                    authority:"Supervisor",
+                    comments:""
+                },
+                {
+                    id:2,
+                    authority:"Head of Department"
+                },
+                {
+                    id:3,
+                    authority:"PS Division"
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+ .v-text-field{
+      width: 500px;
+}
+
+</style>
