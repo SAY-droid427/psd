76ff23a5295bc6213c232a9ef7bb72a4b1730bbb
chetan22 <ck.chetan20@gmail.com>
2021-01-22 17:56:44 +0530

    Added TS2PS api

--- backend/server/apps/tms/serializers.py
+++ backend/server/apps/tms/serializers.py
@@ -6,3 +6,9 @@ class PS2TSTransferSerializer(serializers.ModelSerializer):
     class Meta:
         model = PS2TSTransfer
         fields = ['supervisor_email','hod_email','sub_type','thesis_locale','thesis_subject','name_of_org','expected_deliverables']
+
+class TS2PSTrnasferSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = TS2PSTransfer
+        fields = ['hod_mail', 'sub_type', 'reason_for_transfer', 'name_of_org',]
--- backend/server/apps/tms/views/student_views.py
+++ backend/server/apps/tms/views/student_views.py
@@ -2,8 +2,8 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import APIView
 
-from apps.tms.models import PS2TSTransfer, ActiveUserProfile
-from apps.tms.serializers import PS2TSTransferSerializer
+from apps.tms.models import ActiveUserProfile, PS2TSTransfer, TS2PSTransfer
+from apps.tms.serializers import PS2TSTransferSerializer, TS2PSTrnasferSerializer
 
 
 #without authentication
@@ -25,3 +25,30 @@ class PS2TS(APIView):
         except Exception as e:
             return Response(data=e, status=status.HTTP_400_BAD_REQUEST)
         
+class TS2PS(APIView):
+    def post(self, request, *args, **kwargs):
+        try:
+            active_user = ActiveUserProfile.objects.get(email=request.user.email)
+            if not active_user.is_active_tms:
+                raise Exception('Access Denied. User not present in active user list')
+        except Exception:
+            return Response(
+                data = {
+                    'error': True,
+                    'message': 'Access Denied. User not present in active user list',
+                    'values': {},
+                },
+                status = status.HTTP_403_FORBIDDEN
+            )
+        else:
+            serializer = TS2PSTrnasferSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save(applicant=active_user, cgpa=active_user.cgpa)
+            return Response(
+                data = {
+                    'error': False,
+                    'message': 'TS2PS form saved successfully',
+                    'values': {},
+                },
+                status = status.HTTP_201_CREATED
+            )
